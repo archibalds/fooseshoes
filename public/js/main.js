bb41a3f0696675820896e0d116e5f3a37f56dd7c
archibalds <archibald@email.ua>
2015-10-30 13:34:03 +0200

    add project

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+/*
+ * Custom
+ */
+
+
+$( document ).ready(function() {
+  $(document).on('click', function(event) {
+    if($(event.target).hasClass('menu-icon')) {
+      if($('.menu-icon').hasClass('menu-icon-active')) {
+        $('.menu-icon').removeClass('menu-icon-active');
+        $('.main-menu').hide();
+      } else {
+        $('.menu-icon').addClass('menu-icon-active');
+        $('.main-menu').show();
+      }
+    } else {
+      $('.main-menu').hide();
+      $('.menu-icon').removeClass('menu-icon-active');
+    }
+  });
+});
